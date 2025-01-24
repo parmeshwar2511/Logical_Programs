98b43842f941608f1ddd09e2a53e9427b62c3fda
parmeshwar bogbade <156399286+parmeshwar2511@users.noreply.github.com>
2025-01-24 21:03:41 +0530

    EvenNumber

---
+++ Even_Odd_Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_Odd_Number
+{
+    public class Program
+    {
+         static void Main(string[] args) 
+         {
+         
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is an even number.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is an odd number.");
+            }
+
+            Console.ReadLine();
+         }
+
+    }
+}
+
