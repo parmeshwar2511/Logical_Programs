14f6de0c40cfa8d18c7ce4309437d2d2a3e70ef9
parmeshwar bogbade <parmeshwar2511@gmail.com>
2025-02-03 22:36:42 +0530

    SwappingNumber

---
+++ Daily Task/Swaping_Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        int a = 22;
+//        int b = 40;
+//        Console.WriteLine($"a : {a} b : {b}");
+//        a = a + b;
+//        b = a - b;
+//        a = a - b;
+//        Console.WriteLine($"After swap a: {a}");
+//        Console.WriteLine($"After swap b : {b}");
+
+//        Console.ReadLine();
+//    }
+//}
+
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter two numbers that u want to swap Number");
+        int a = int.Parse(Console.ReadLine());
+
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine($"the numbers before swapping a:{a} and b:{b}");
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        Console.WriteLine($"the numbers after swapping a:{a} and b:{b}");
+    }
+}
