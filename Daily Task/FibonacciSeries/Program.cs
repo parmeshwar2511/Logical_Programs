d97c8da6b0a9abd69cd04b95cc63721d6a61f280
parmeshwar bogbade <parmeshwar2511@gmail.com>
2025-02-17 00:20:43 +0530

    logical

---
+++ Daily Task/FibonacciSeries/Program.cs
@@ -0,0 +1,30 @@
+﻿
+public class program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Enter the number ");
+        int n = int.Parse(Console.ReadLine());
+
+
+        Console.WriteLine("Fibonacci Sequence:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(Fibonum(i));
+        }
+    }
+
+
+    static int Fibonum(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        else
+        {
+            return Fibonum(n - 1) + Fibonum(n - 2);
+        }
+    }
+}
